adfb7e86052a2ab7e91efa11744a93199beadcc2
Anabel <villalobos1998@gmail.com>
2023-03-25 23:21:05 -0700

    completed 07.Cameras

--- 07.Cameras/src/script.js
+++ 07.Cameras/src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls' 
 
 /**
  * Base
@@ -6,9 +7,17 @@ import * as THREE from 'three'
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
+
 // Curser
+const cursor = {
+    x: 0,
+    y: 0
+}
+
+
 window.addEventListener('mousemove', (e) => {
-    console.log(e.clientX)
+    cursor.x = e.clientX / sizes.width - 0.5;
+    cursor.y = - (e.clientY / sizes.height - 0.5);
 })
 
 
@@ -45,6 +54,12 @@ camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
 
+//Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+
+
 // Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
@@ -60,6 +75,15 @@ const tick = () =>
 
     // Update objects
     // mesh.rotation.y = elapsedTime;
+
+    //Update camera
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2
+    // camera.position.y = cursor.y * 3
+    // camera.lookAt(mesh.position)
+
+    //Update controls
+    controls.update()
 
     // Render
     renderer.render(scene, camera)
